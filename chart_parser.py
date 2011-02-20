4d921297ef119a1fddeb66b841935c412c342ae7
Taliesin Beynon <taliesinb@gmail.com>
2011-02-20 17:00:07 -0500

    Print code moved into chart, symbols now know their span. New parse method to return list of winners.

--- chart_parser.py
+++ chart_parser.py
@@ -1,64 +1,62 @@
 #!/usr/bin/python
-from copy import copy
 from collections import defaultdict
 from sys import setrecursionlimit
 
 setrecursionlimit(64)
 
 class symbol:
-  def __init__(self, type, value):
+  def __init__(self, type, value, span):
     self.type = type
     self.value = value
+    self.span = span
     
   def __repr__(self):
     return str(self.value)
 
 def copy_fragment(frag):
-  return fragment(frag.chart, frag.left, frag.right, frag.pattern, frag.action, frag.matched, frag.type)
+  return fragment(frag.chart, frag.span, frag.pattern, frag.action, frag.matched, frag.type)
   
 def new_fragment(chart, pos, type, pattern, action):
-  print "new with pattern", pattern
-  return fragment(chart, pos, pos, pattern, action, [], type)
+  return fragment(chart, [pos,pos], pattern, action, [], type)
 
 class fragment:    
-  def __init__(self, chart, left, right, pattern, action, matched, type):
+  def __init__(self, chart, span, pattern, action, matched, type):
     self.chart = chart
-    self.left = left
-    self.right = right
+    self.span = span[:]
     self.action = action
-    self.pattern = copy(pattern)
-    self.matched = copy(matched)
+    self.pattern = pattern[:]
+    self.matched = matched[:]
     self.type = type
     
   def next_type(self):
     return self.pattern[0]
     
   def next_pos(self):
-    return self.right
+    return self.span[1]
                 
-  def grow(self, sym, pos):
+  def grow(self, sym):
     print "fragment of type", self.type, "growing using symbol", sym.type
-    assert self.right == pos[0]
+    assert self.next_pos() == sym.span[0]
     assert self.pattern
     assert sym.type == self.pattern.pop(0)
     self.matched.append(sym)
-    self.right = pos[1]
+    self.span[1] = sym.span[1]
     if self.pattern: 
       self.chart.add_edge(self)
       self.chart.predict(self.next_pos(), self.next_type())
     else:
-      self.chart.add_symbol((self.left, self.right), symbol(self.type, self.action(*self.matched)))
+      self.chart.add_symbol(symbol(self.type, self.action(*self.matched), self.span))
       print "finished", self.type, "by matching", [m.type for m in self.matched]
   
   def __repr__(self):
     return self.type + ":\t" + (' '.join(map(lambda x: str(x.type), self.matched)) + ' | ' + ' '.join(map(str,self.pattern))).strip() + '\t\t' + ','.join(map(lambda x: str(x.value),self.matched))
-    return "<" + ','.join(map(str, [self.type, self.left, self.right, self.matched])) + '>'
+    return "<" + ','.join(map(str, [self.type, self.span, self.matched])) + '>'
     
   def __hash__(self):
-    return hash(self.type) + sum(map(hash, self.matched)) + hash(self.left) + hash(self.right)
+    return hash(self.type) + sum(map(hash, self.matched)) + hash(self.span[0]) + hash(self.span[1])
     
   def __eq__(self, other):
-    return self.type == other.type and self.left == other.left and self.right == other.right and self.matched == other.matched
+    return self.type == other.type and self.span == other.span and self.matched == other.matched
               
 # edge 
 class chart:
@@ -67,18 +65,23 @@ class chart:
     self.edges = defaultdict(set)
     self.rules = rules
     self.predict(0, "start")
-    print "prediction finished, adding symbols"
-    print
-    for (l, r, sym) in tokens:
-      self.add_symbol((l,r), sym)
     
+  def parse(self, tokens):
+    rightmost = max(t.span[1] for t in tokens)
+    for token in tokens:
+      self.add_symbol(token)
+    winners = [symbol 
+      for symbol in ch.symbols[(0, "start")] 
+      if symbol.span[1] == rightmost]
+    return winners
+        
   def add_edge(self, frag):
     print "adding edge for:", frag.type, "at", frag.next_pos(), "on", frag.next_type()
     type = frag.next_type()
     pos = frag.next_pos()
     self.edges[(pos,type)].add(frag)
     for symbol in self.symbols[(pos,type)]:
-      copy_fragment(frag).grow(symbol, pos)
+      copy_fragment(frag).grow(symbol)
     
   def predict(self, pos, type):
     for pattern, action in rules.get(type,{}):
@@ -89,20 +92,31 @@ class chart:
       if predict:        
         self.predict(pos, first_symbol)
   
-  def add_symbol(self, pos, symbol):
-    print "adding symbol", symbol.type, "at", pos
-    key = (pos[0], symbol.type)
+  def add_symbol(self, symbol):
+    print "adding symbol", symbol.type, "at", symbol.span
+    key = (symbol.span[0], symbol.type)
     self.symbols[key].add(symbol)
-    for frag in self.edges[(pos[0], symbol.type)]:
-      print "symbol triggered edge"
-      copy_fragment(frag).grow(symbol, pos) 
+    for frag in self.edges[key]:
+      print "symbol triggered edge:\t", frag
+      copy_fragment(frag).grow(symbol) 
+      
+  def print_edges(self):
+    for pos, edge in sorted(ch.edges.items()):
+      if edge: 
+        print pos, "\n\t", '\n\t'.join(map(str,edge))
+
+  def print_symbols(self):
+    for key, symbol in ch.symbols.items():
+      print key, "\t->\t", symbol
+    
     
 tokens = [
-  (0,1, symbol("a", "A1")),
-  (1,2, symbol("a", "A2")),  
-  (2,3, symbol("a", "A3")),
-  (3,4, symbol("b", "B1")),
-  (4,5, symbol("b", "B2"))]
+  symbol("a", "A1", (0,1)),
+  symbol("a", "A2", (1,2)),  
+  symbol("a", "A3", (2,3)),
+  symbol("b", "B1", (3,4)),
+  symbol("b", "B2", (4,5))
+]
   
 rules = {}
 rules["start"] = [(["A", "B"], lambda x, y: (x,y))]
@@ -110,24 +124,21 @@ rules["A"] = [(["A", "A"], lambda x, y: (x,y)), (["a"], lambda x: "a")]
 rules["B"] = [(["a", "b", "b"], lambda x, y, z: (x,y,z)), (["a"], lambda x: "a")]
 
 ch = chart(tokens, rules)
-print
-if (0, "start") in ch.symbols:
-  print "winning symbols:"
-  for win in ch.symbols[(0, "start")]:
-    print win
-else:
-  print "full chart:"
-  for key, symbol in ch.symbols.items():
-    print key[0], " -> ", symbol
 
-print
-print "edges:"
-for pos, edge in sorted(ch.edges.items()):
-  if edge: 
-    print pos, "\n\t", '\n\t'.join(map(str,edge))
+winners = ch.parse(tokens)
+
+print "winners:"
+for w in winners:
+  print "\t", winners
+
+if not winners:  
   print
+  print "chart:"
+  ch.print_symbols()
 
-print "done"
+  print
+  print "edges:"
+  ch.print_edges()
 
 # artisan asylum: tuesday
 # sprout: thursday      
