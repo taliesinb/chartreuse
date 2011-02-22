8ac04eb29c81c13b56e2d11c8d86f878547b5bd3
Taliesin Beynon <taliesinb@gmail.com>
2011-02-21 23:01:15 -0500

    Mostly moved over, though interaction with optimization is weird

--- compile_grammar.py
+++ compile_grammar.py
@@ -5,86 +5,96 @@ from itertools import combinations, groupby
 rules = defaultdict(list)
 names = defaultdict(int)
 
-# create a new unique name based on original name
-def new_name(name):
-  names[name] += 1  
-  return name + "" + str(names[name])
-  
+def tosym(x):
+  if type(x) == str:
+    return symbol(x)
+  else:
+    return x
+    
+def tosyms(exps):
+  return map(tosym, exps)
+    
 def tostr(arg):
   if type(arg) in [list, tuple]:
     return ''.join(map(tostr, arg))
   else:
     return str(arg)
+
+def next_name(name):
+  names[name] += 1
+  return join_name(name, names[name])
   
 def join_name(*names):
   return  ''.join(map(tostr, names))
-
-# is this term an optional term (= a singleton tuple)
-def optionalQ(term):
-  return type(term) == tuple and len(term) == 1
   
-from itertools import groupby
-
-# handle a term that is a list of consecutive clauses
-def dolist(terms, name):
-  rules[name] = [[doterm(symbol, new_name(name)) for symbol in terms]]
-  return name
-
-# handle a term that is a list of alternative clauses      
-def doalt(terms, name):
-  for term in terms:
-    rules[name].append(term)
-  return name
-  
-def doopt(term, name):
-  rules[name] = [[], doterm(term, name)]
-  return name
-
-# handle a term that is a bag-of-clauses, returning its name
-def dobag(terms, name):
-  length = len(terms)
-  valid_super_names = []
-  unit_names = [join_name(name, k) for k in range(length)]
-
-  for i in range(2, length+1):
-    for perm in combinations(range(length), i):
-      super_name = join_name(name, perm)
-      if 0 in perm:
-        valid_super_names.append(super_name)
-      sub_rules = set()
-      for k in range(length):
-        if k in perm:
-          sub_perm = list(perm)
-          sub_perm.remove(k)
-          sub_name = join_name(name, sub_perm)
-          sub_rules.add((sub_name, unit_names[k]))
-          sub_rules.add((unit_names[k], sub_name))
-      rules[super_name] = map(list, list(sub_rules))
-      
-  rhs = [doterm(symbol, new_name(name)) for symbol in terms.keys()]
-  
-  for unit_name, term in zip(unit_names, rhs):
-    rules[unit_name].append(term)
+class symbol:
+  def __init__(self, name):
+    self.name = name
     
-  valid_super_names.append(join_name(name, "0"))
-  rules[name] = valid_super_names
-  return name
+  def compile(self, name):
+    return self.name
+
+class seq:
+  def __init__(self, *args):
+    self.terms = tosyms(args)
+    
+  def compile(self, name):
+    name += "S"
+    rules[name] = [[term.compile(next_name(name)) for term in self.terms]]
+    return name
+
+class alt:
+  def __init__(self, *opts):
+    self.terms = tosyms(opts)
         
-# dispatch on a term according to its type
-def doterm(term, name):
-  t = type(term)
-  if t == list:
-    return dolist(term, name + "L")
-  if t == tuple:
-    if len(term) == 1:
-      return doopt(term[0], name + "?")
-    else:
-      return doalt(term, name + "A")
-  if t == dict:
-    return dobag(term, name + "B")
-  if t == str:
-    return term
-         
+  def compile(self, name):
+    name += "A"
+    rules[name] = [term.compile(next_name(name)) for term in self.terms]
+    return name
+  
+class opt:
+  def __init__(self, term):
+    self.term = tosym(term)
+
+  def compile(self, name):
+    name += "?"
+    rules[name] = ["_empty_", self.term.compile(name)]
+    return name
+    
+class bag:
+  def __init__(self, **args):
+    self.terms = tosyms(args)
+    
+  def compile(self, name):
+    name += "B"
+    length = len(self.terms)
+    valid_super_names = []
+    unit_names = [join_name(name, k) for k in range(length)]
+
+    for i in range(2, length+1):
+      for perm in combinations(range(length), i):
+        super_name = join_name(name, perm)
+        if 0 in perm:
+          valid_super_names.append(super_name)
+        sub_rules = set()
+        for k in range(length):
+          if k in perm:
+            sub_perm = list(perm)
+            sub_perm.remove(k)
+            sub_name = join_name(name, sub_perm)
+            sub_rules.add((sub_name, unit_names[k]))
+            sub_rules.add((unit_names[k], sub_name))
+        rules[super_name] = map(list, list(sub_rules))
+        
+    rhs = [term.compile(name) for symbol in terms.keys()]
+    
+    for unit_name, term in zip(unit_names, rhs):
+      rules[unit_name].append(term)
+      
+    valid_super_names.append(join_name(name, "0"))
+    rules[name] = valid_super_names
+    return name
+        
 # recursively replace all instances in a list-tree according to dictionary "reps"
 def replace_in_list(x, reps):
   if type(x) == list:
@@ -124,10 +134,12 @@ def optimize():
     for a,b in rewrite.items():
       print "\t", a.ljust(20), "->", b
 
+def define(name, expr):
+  rules[name].append(expr.compile(name))
+
 # test a fixed list containing various other types of clauses
-expr = ["a", {"j1":"foo", "j2":"bar", "j3":"baz"}, ["c","d"], ("e","f"), ("g",), "h","i", (["j1","j2"],)]
-expr = ["one","1.5","1.6","1.7", ("two?",), ("2.5?",), "three", "four", (["five1","five2"],)]
-dolist(expr, "start")
+expr = seq("a", "b", seq("c", "d"), opt("e"), "g", "h")
+define("start", expr)
 
 print expr
 print
